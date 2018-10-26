741aed4d722126b3caccc2dd064e157d3a6e0215
https://github.com/GaryLeeeee/Fellowship-activity.git <664306561@qq.com>
2018-10-27 00:12:51 +0800

    完成反射调用method(自动匹配参数)

--- src/main/java/com/garylee/framework/controller/HelloController.java
+++ src/main/java/com/garylee/framework/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.garylee.framework.controller;
 
 import com.garylee.framework.annotation.Controller;
 import com.garylee.framework.annotation.RequestMapping;
+import com.garylee.framework.annotation.ResponseBody;
 
 /**
  * Created by GaryLee on 2018-10-25 23:12.
@@ -11,5 +12,38 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
+    }
+    @RequestMapping("/getName")
+    @ResponseBody
+    public String getName(){
+        return "GaryLee";
+    }
+    @RequestMapping("/count")
+    @ResponseBody
+    public int count(int a,int b){
+        System.out.println("a+b="+a+b);
+        return a+b;
+    }
+    @RequestMapping("/add")
+    @ResponseBody
+    public String add(String a,String b){
+        System.out.println("a+b="+a+b);
+        return a+b;
+    }
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login(String username,String password){
+        if(username.equals("root")&&password.equals("admin"))
+            return "success";
+        else
+            return "fail";
+    }
+    @RequestMapping(value = "/post",method = "post")
+    @ResponseBody
+    public String post(String username,String password){
+        if(username.equals("root")&&password.equals("admin"))
+            return "success";
+        else
+            return "fail";
     }
 }
--- src/main/java/com/garylee/framework/handler/MappingHandler.java
+++ src/main/java/com/garylee/framework/handler/MappingHandler.java
@@ -8,8 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -24,15 +27,88 @@ public class MappingHandler {
             Class clazz = classMap.get(uri);
             Object object = clazz.newInstance();
             Map<String, String[]> map = req.getParameterMap();
-            String fileName = methodMap.getMethod().invoke(object).toString();
             if (!methodMap.isBody()) {
+                String fileName = methodMap.getMethod().invoke(object).toString();
                 if (htmlMap.containsKey(fileName)){
                     resp.setContentType("text/html; charset=UTF-8");//乱码加上这句
                     resp.getWriter().write(htmlMap.get(fileName));
                 }
+            }else if (methodMap.isBody()){
+                //获取反射方法的参数类型(方便转化)
+                Class<?>[] classz = methodMap.getMethod().getParameterTypes();
+                //获取反射方法的形参名(方便获取value的值--map实现)
+                //此处需1.8编译版本,与javac -parameters,否则返回值均为arg0,arg1...
+                Parameter[] parameters = methodMap.getMethod().getParameters();
+                //存放参数值,按顺序
+                //如int a,int b对应的是2,3
+                List<Object> objects = new ArrayList<>();
+                for(int i=0;i<classz.length;i++) {
+                    fillList(objects, classz[i], map.get(parameters[i].getName())[0]);//默认只有一个参数
+                }
+                //调用,返回o是结果(return)
+                //invoke参数为class对象和参数值数组
+                Object o = methodMap.getMethod().invoke(object,objects.toArray());
+//                System.out.println("返回的object:"+o);
+                resp.getWriter().print(o);
             }
         }catch (Exception e){
             e.printStackTrace();
+            //如果参数传少了就返回错误信息
+            try {
+                resp.setContentType("text/html; charset=UTF-8");//乱码加上这句
+                resp.getWriter().print("<h1>参数缺失!</h1>");
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    //将map获取到的值转化为方法参数的类型,并按顺序存放到list
+    private static void fillList(List<Object> list, Class<?> parameter,Object value) {
+//        System.out.println(parameter.getName());
+        if("java.lang.String".equals(parameter.getName())){
+            list.add(value);
+        }else if("java.lang.Character".equals(parameter.getName())){
+            char[] ch = ((String)value).toCharArray();
+            list.add(ch[0]);
+        }else if("char".equals(parameter.getName())){
+            char[] ch = ((String)value).toCharArray();
+            list.add(ch[0]);
+        }else if("java.lang.Double".equals(parameter.getName())){
+            list.add(Double.parseDouble((String) value));
+        }else if("double".equals(parameter.getName())){
+            list.add(Double.parseDouble((String) value));
+        }else if("java.lang.Integer".equals(parameter.getName())){
+            list.add(Integer.parseInt((String) value));
+        }else if("int".equals(parameter.getName())){
+            list.add(Integer.parseInt((String) value));
+        }else if("java.lang.Long".equals(parameter.getName())){
+            list.add(Long.parseLong((String) value));
+        }else if("long".equals(parameter.getName())){
+            list.add(Long.parseLong((String) value));
+        }else if("java.lang.Float".equals(parameter.getName())){
+            list.add(Float.parseFloat((String) value));
+        }else if("float".equals(parameter.getName())){
+            list.add(Float.parseFloat((String) value));
+        }else if("java.lang.Short".equals(parameter.getName())){
+            list.add(Short.parseShort((String) value));
+        }else if("shrot".equals(parameter.getName())){
+            list.add(Short.parseShort((String) value));
+        }else if("java.lang.Byte".equals(parameter.getName())){
+            list.add(Byte.parseByte((String) value));
+        }else if("byte".equals(parameter.getName())){
+            list.add(Byte.parseByte((String) value));
+        }else if("java.lang.Boolean".equals(parameter.getName())){
+            if("false".equals(value) || "0".equals(value)){
+                list.add(false);
+            }else if("true".equals(value) || "1".equals(value)){
+                list.add(true);
+            }
+        }else if("boolean".equals(parameter.getName())){
+            if("false".equals(value) || "0".equals(value)){
+                list.add(false);
+            }else if("true".equals(value) || "1".equals(value)){
+                list.add(true);
+            }
         }
     }
 }
